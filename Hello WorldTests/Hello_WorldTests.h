2af9bf2a10cd7139c74ce2c74de9cef8aa1ddc86
tuanpham <tuanpham@cnc.com.vn>
2013-06-04 11:03:35 +0700

    Initial Commit

---
+++ Hello WorldTests/Hello_WorldTests.h
@@ -0,0 +1,13 @@
+//
+//  Hello_WorldTests.h
+//  Hello WorldTests
+//
+//  Created by cncsoft on 6/4/13.
+//  Copyright (c) 2013 cncsoft. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Hello_WorldTests : SenTestCase
+
+@end
---
+++ Hello WorldTests/Hello_WorldTests.m
@@ -0,0 +1,32 @@
+//
+//  Hello_WorldTests.m
+//  Hello WorldTests
+//
+//  Created by cncsoft on 6/4/13.
+//  Copyright (c) 2013 cncsoft. All rights reserved.
+//
+
+#import "Hello_WorldTests.h"
+
+@implementation Hello_WorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Hello WorldTests");
+}
+
+@end
